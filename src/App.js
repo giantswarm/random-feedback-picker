d08c62850c48bbc7d1cf614d4574f93a1e3ef626
Oliver Ponder <oliver.ponder@gmail.com>
2017-12-14 07:28:40 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import './App.css';
+import TeamMembers from './team';
+import _ from 'underscore';
+import { shuffle } from 'shuffle-seed';
+
+// var seed = "SEEDYOL";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      seed: window.location.href.split('#')[1],
+      working: false
+    };
+  }
+
+  assignMembers(allMembers, seed) {
+    _.map(_.groupBy(allMembers, "team"), ((teamMembers, teamName) => {
+      _.each(teamMembers, (member, i) => {
+        member.firstAssignment = shuffle(teamMembers, seed)[teamMembers.indexOf(member)];
+        member.secondAssignment = shuffle(allMembers, seed)[allMembers.indexOf(member)];
+      });
+    }))
+
+    return allMembers;
+  }
+
+  // Determines if the result of assignMembers is a valid distribution.
+  // (i.e.
+  //    nobody is set to give feedback on themselves
+  //    nobody is set to give feedback on a team-member for the second assignment
+  //  )
+  isValidOutcome(allMembers) {
+    var invalidMember = _.find(allMembers, (member) => {
+      return member.firstAssignment === member ||
+             member.firstAssignment.team !== member.team ||
+             member.secondAssignment.team === member.team
+    });
+
+    return !invalidMember;
+  }
+
+  randomCode() {
+    return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+  }
+
+  generateValidSeed() {
+    this.setState({
+      working: true
+    }, () => {
+      setTimeout(() => {
+        var seed = this.randomCode();
+        var valid = false
+        while (!valid) {
+          seed = this.randomCode();
+          valid = this.isValidOutcome(this.assignMembers(TeamMembers, seed))
+        }
+
+        window.location.hash = seed;
+
+        this.setState({
+          valid: true,
+          working: false,
+          seed: seed
+        });
+      }, 20)
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <h1 className="App-title">Giant Swarm Feedback Picker</h1>
+            <p>
+              This app helps us determine who we will give feedback to in the Quarterly Peer Review.
+            </p>
+
+            <p>
+              Press the big green button to find out which colleagues you'll be giving feedback to.
+            </p>
+          </div>
+
+          <div>
+            {
+              this.state.working ?
+              <img src="img/loader.svg" height="50px" width="88px" style={{display: "block"}}/>
+              :
+              <button onClick={this.generateValidSeed.bind(this)}>GO</button>
+            }
+
+            <div className="seed-explanation">
+              Seed: {this.state.seed}<br/>
+              Valid: {this.state.valid ? "Yes" : "No"}<br/>
+            </div>
+          </div>
+        </header>
+        <div className="teams">
+          {
+            _.map(_.groupBy(this.assignMembers(TeamMembers, this.state.seed), "team"), ((members, teamName) => {
+              return <div className="team" key={teamName}>
+                <h2>{teamName}</h2>
+
+                {_.map(members, (member) => {
+                  return <div className="member" key={member.name}>
+                    <img src={"img/team/" +member.name + ".jpg"} alt={member.name} />
+                    <h3>{member.name}</h3>
+
+                    <div className="feedback-assignments">
+                      <img src={"img/team/" + member.firstAssignment.name +".jpg"} alt="o" />
+                      <img src={"img/team/" + member.secondAssignment.name + ".jpg"} alt="o" />
+                    </div>
+                  </div>
+                })}
+              </div>
+            }))
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/team.js
@@ -0,0 +1,31 @@
+var teamMembers = [
+  // Batman
+  {"name": "Federico Gimenez",        "team": "batman"},
+  {"name": "Fernando Ripoll",         "team": "batman"},
+  {"name": "Oliver Ponder",           "team": "batman"},
+  {"name": "Puja Abbassi",            "team": "batman"},
+  {"name": "Ross Fairbanks",          "team": "batman"},
+  {"name": "Volodymyr Stoiko",        "team": "batman"},
+
+  // Magic
+  {"name": "Joseph Salisbury",        "team": "magic"},
+  {"name": "Julien Garcia Gonzalez",  "team": "magic"},
+  {"name": "Marian Steinbach",        "team": "magic"},
+  {"name": "Paweł Kopiczko",          "team": "magic"},
+  {"name": "Roman Sokolkov",          "team": "magic"},
+
+  // Rainmakers
+  {"name": "Anna Loew",               "team": "rainmakers"},
+  {"name": "Henning Lange",           "team": "rainmakers"},
+  {"name": "Oliver Thylmann",         "team": "rainmakers"},
+  {"name": "Thea Hüttenberger",       "team": "rainmakers"},
+
+  // Spirit
+  {"name": "Marcel Müller",           "team": "spirit"},
+  {"name": "Tim Schindler",           "team": "spirit"},
+  {"name": "Timo Derstappen",         "team": "spirit"},
+  {"name": "Tobias Bradtke",          "team": "spirit"},
+  {"name": "Vaclav Rozsypalek",       "team": "spirit"},
+]
+
+export default teamMembers;
